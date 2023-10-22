4bc26f85803101751319bf2c4f74437175baa09a
moabukar <mo-abukar10@hotmail.com>
2023-10-22 01:07:15 +0100

    add Py implementation of ZeroMQ

---
+++ py/server.py
@@ -0,0 +1,15 @@
+import zmq
+
+# ZeroMQ Context
+context = zmq.Context()
+
+# Define the socket using the "Context"
+sock = context.socket(zmq.REP)
+sock.bind("tcp://127.0.0.1:5678")
+
+# Run a simple "Echo" server
+while True:
+    message = sock.recv_string()
+    sock.send_string("Echo: " + message)
+    print("Echo: Test " + message)
+
